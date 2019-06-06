9fd852957f665d059f2fd65975ce5cefda021e48
Gilda Snow <gilda.snow@gmail.com>
2019-06-06 04:50:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,9 +72,10 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.map do |season, holidays|
   holidays.map do |holiday, supplies|
-    holiday if supplies.include?("BBQ")
+    if supplies.include?("BBQ")
+      return holiday.flatten
   end
-end.flatten
+end
 end
 
 
