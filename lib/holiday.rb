77e1cf4a4b26933dfe18b7d37732650cd517b066
Gilda Snow <gilda.snow@gmail.com>
2019-06-06 03:58:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    holidays.each do |
+    holidays.each do |holiday, supplies|
+      
 
 end
 
